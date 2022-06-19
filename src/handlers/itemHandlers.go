8f975fdd932327a4b069807d1b0d0598ab082d23
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-19 15:56:33 +0200

    added the GetAllItems controller functionality

--- src/handlers/itemHandlers.go
+++ src/handlers/itemHandlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"shoppinglist/src/data"
 	"sync"
@@ -12,5 +13,25 @@ type ItemHandlers struct {
 }
 
 func (h * ItemHandlers) GetAllItems(w http.ResponseWriter, r *http.Request){
-  w.Write([]byte("Hello world"))
+
+  items := make([]data.Item, len(h.store))
+
+  i := 0
+  h.Lock()
+    for _, item := range h.store {
+    items[i] = item
+    i++
+  }
+  h.Unlock()
+
+  jsonBytes, err := json.Marshal(items)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte(err.Error()))
+    return
+  }
+
+  w.Header().Add("content-type", "application/json")
+  w.Write(jsonBytes)
 }
