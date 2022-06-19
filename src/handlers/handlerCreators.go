d674954dec07957be4571e2e04c1fd52a7bec87f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-19 14:55:16 +0200

    added the ItemHandlerCreator

---
+++ src/handlers/handlerCreators.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "shoppinglist/src/data"
+
+func createItemHandler() *ItemHandlers {
+	return &ItemHandlers{
+		store: map[string]data.Item{
+			"1655570749194813500": {
+				Id:       "1655570749194813500",
+				Name:     "Carrots",
+				Quantity: 10,
+			},
+		},
+	}
+}
