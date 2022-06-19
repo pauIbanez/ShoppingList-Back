f654dc0eb4bd9f790e1877472fb918a4e2e86179
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-19 20:18:53 +0200

    added the /items/delete endpoint pointing to the delete handler

--- src/server.go
+++ src/server.go
@@ -11,6 +11,7 @@ func StartServer(port string) {
   http.HandleFunc("/items/all", itemhandlers.GetAllItems)
   http.HandleFunc("/items/modify", itemhandlers.ModifyItem)
   http.HandleFunc("/items/create", itemhandlers.CreateItem)
+  http.HandleFunc("/items/delete", itemhandlers.DeleteItem)
 
   err := http.ListenAndServe(":"+port, nil)
   if err != nil {
